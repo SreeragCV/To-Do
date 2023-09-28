46c3d2bd61f7505bfb0bcac9f133eff88ec663b2
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-09-28 16:23:05 +0530

    useState Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+
   return (
-    <div className="App">
+    <div className="app">
       
       <div className="mainHeading">
-        <h1>ToDo List</h1>
+        <h1>To-Do List</h1>
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Go on, Add your LIST! 🌝☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={ e => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, toDo])} className="fas fa-plus"></i>
       </div>
+      {toDos.map((obj) => {
+        return(
       <div className="todos">
         <div className="todo">
           <div className="left">
             <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <p>{obj}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
         </div>
       </div>
-    </div>
+        )
+      } 
+    )}
+  </div>
   );
 }
 
